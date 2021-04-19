3e063db6080c6922f722923a4f8122a8035a1457
Brendan OConnell <bkoconnell@gmail.com>
2021-04-19 18:53:53 -0400

    project one changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://5kcyqvhl87.execute-api.us-east-1.amazonaws.com' // deployed API URL (without the '/api' at the end)
 };
